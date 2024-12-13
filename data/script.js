9829e988c402d01d7f3cf90f108595d2c60c2687
gwen1008 <gwenwu2004@gmail.com>
2024-12-14 03:07:59 +0800

    DONE

--- data/script.js
+++ data/script.js
@@ -1,21 +1,12 @@
 // 定義圖片路徑陣列
-const images = [
-    'images/pic1.jpg',
-    'images/pic2.jpg',
-    'images/pic3.jpg',
-    'images/pic4.jpg',
-    'images/pic5.jpg',
-    'images/pic6.jpg',
-    'images/pic7.jpg',
-    'images/pic8.jpg',
-    'images/pic9.jpg',
-    'images/pic10.jpg',
-    'images/pic11.jpg',
-    'images/pic12.jpg',
-    'images/pic13.jpg',
-    'images/pic14.jpg',
-    'images/pic15.jpg'
-];
+const images = [];
+
+for (let i = 1; i <= 65; i++) {
+    images.push(`images/pic${i}.jpg`);
+}
+
+console.log(images);
+
 
 let currentImageIndex = 0;  // 當前顯示的圖片索引
 
